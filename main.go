52d220cb87847a298c11a1c1986a6e3e7f289cf0
Phillipa <pbennetteghan.work@gmail.com>
2026-05-18 15:49:04 +0000

    moved appropriate code into main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("listening on :8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		fmt.Println(conn)
+		conn.Close()
+	}
+}
