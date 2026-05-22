ee372364356f7e15b7da384913dc1468ebbcb48f
Phillipa <pbennetteghan.work@gmail.com>
2026-05-22 01:57:46 +0000

    handler for os interrupt

--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 )
+
+func handleConn(conn net.Conn) {
+	addr := conn.RemoteAddr().String()
+
+	log.Printf("connect: %s", addr)
+	defer log.Printf("disconnect: %s", addr)
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Print(line)
+
+		conn.Write([]byte(line))
+
+		if line == "\r\n" {
+			break
+		}
+	}
+	conn.Close()
+}
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
@@ -12,6 +40,15 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("listening on :8080")
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		<-ctx.Done()
+		fmt.Println("\nreceived interrupt, closing listener......")
+		listener.Close()
+	}()
 
 	for {
 		conn, err := listener.Accept()
